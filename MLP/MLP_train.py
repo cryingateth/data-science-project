dbab1e57d3af7959a14c1a41ff0ff7289cdb45f1
VitoPagone <cristianpagone1@gmail.com>
2023-05-30 07:49:27 +0200

    MLP new code for hyperparameter tuning

--- MLP/MLP_train.py
+++ MLP/MLP_train.py
@@ -1,12 +1,13 @@
 import copy
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
 
 from Architecture import MLP
 from Dataset import BRCA_Dataset
@@ -17,10 +18,12 @@ TARGET_COLUMN = 'BRCA_subtype'
 TEST_SIZE = 0.3
 VALIDATION_TEST_SIZE = 0.2
 RANDOM_STATE = 42
-LEARNING_RATE = 0.001
-BATCH_SIZE = 64
 NUM_EPOCHS = 50
 MODEL_PATH = 'best_model.pt'
+
+# Possible hyperparameters
+LEARNING_RATES = [0.1, 0.01, 0.001]
+BATCH_SIZES = [32, 64, 128]
 
 # Function to calculate loss
 def calculate_loss(loader, model):
@@ -32,19 +35,6 @@ def calculate_loss(loader, model):
             loss = criterion(outputs, labels)
             total_loss += loss.item()
     return total_loss / len(loader)
-
-# Function to calculate accuracy
-def calculate_accuracy(loader, model):
-    total_samples = 0
-    correct_samples = 0
-    model.eval()
-    with torch.no_grad():
-        for features, labels in loader:
-            outputs = model(features)
-            _, predicted = torch.max(outputs.data, 1)
-            total_samples += labels.size(0)
-            correct_samples += (predicted == labels).sum().item()
-    return correct_samples / total_samples
 
 # Load and process data
 data = pd.read_csv(CSV_FILE_PATH)
@@ -65,51 +55,66 @@ train_dataset = BRCA_Dataset(X_train, y_train)
 val_dataset = BRCA_Dataset(X_val, y_val)
 test_dataset = BRCA_Dataset(X_test, y_test)
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-
 # Model instantiation
 input_size = X_train.shape[1]
 output_size = len(np.unique(y_train))
 
-model = MLP(input_size, output_size)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-
-best_model_wts = copy.deepcopy(model.state_dict())
+best_model_wts = None
 best_loss = np.inf
-train_losses = []
-val_losses = []
+best_lr = None
+best_batch_size = None
 
-# Training loop
-for epoch in range(NUM_EPOCHS):
-    model.train()
-    for i, (inputs, labels) in enumerate(train_loader):
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
+for LEARNING_RATE in LEARNING_RATES:
+    for BATCH_SIZE in BATCH_SIZES:
+        # Model instantiation
+        model = MLP(input_size, output_size)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        # Prepare data loaders
+        train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-    val_loss = calculate_loss(val_loader, model)
+        # Training loop
+        train_losses = []
+        val_losses = []
+        for epoch in range(NUM_EPOCHS):
+            model.train()
+            for i, (inputs, labels) in enumerate(train_loader):
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
 
-    if val_loss < best_loss:
-        best_loss = val_loss
-        best_model_wts = copy.deepcopy(model.state_dict())
-        torch.save(model.state_dict(), MODEL_PATH)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-    train_loss = calculate_loss(train_loader, model)
-    train_losses.append(train_loss)
-    val_losses.append(val_loss)
+            train_loss = calculate_loss(train_loader, model)
+            val_loss = calculate_loss(val_loader, model)
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
 
-    print(f'Epoch {epoch+1}/{NUM_EPOCHS}, Training Loss: {train_loss}, Validation Loss: {val_loss}')
+            print(f'Epoch {epoch+1}/{NUM_EPOCHS}, Training Loss: {train_loss}, Validation Loss: {val_loss}')
 
-# Load best model weights
-model.load_state_dict(best_model_wts)
+            if val_loss < best_loss:
+                best_loss = val_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+                best_lr = LEARNING_RATE
+                best_batch_size = BATCH_SIZE
+                torch.save(model.state_dict(), MODEL_PATH)
 
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+        plt.figure()
+        plt.plot(train_losses, label='Training Loss')
+        plt.plot(val_losses, label='Validation Loss')
+        plt.title(f'Training and Validation Losses with LR: {LEARNING_RATE}, Batch Size: {BATCH_SIZE}')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.savefig(f'loss_plot_lr{LEARNING_RATE}_bs{BATCH_SIZE}.png')
+
+        print(f'Finished Training with LR: {LEARNING_RATE}, Batch Size: {BATCH_SIZE}, Best Validation Loss: {best_loss}')
+
+print(f'Best Hyperparameters - Learning Rate: {best_lr}, Batch Size: {best_batch_size}')
 
 # Load best model weights
 model.load_state_dict(best_model_wts)
@@ -129,4 +134,3 @@ with torch.no_grad():
 print(f'Accuracy: {accuracy_score(y_true, y_pred)}')
 print(f'Confusion Matrix: \n{confusion_matrix(y_true, y_pred)}')
 print(f'Classification Report: \n{classification_report(y_true, y_pred)}')
-
